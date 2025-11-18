7528572c5fb30c0bfe52063d87eb3b3802368bc4
LeviathanST <phamquochung170505@gmail.com>
2025-11-18 12:48:19 +0700

    seed.sql: update

--- Sepsis-App-project/backend/sql/seed.sql
+++ Sepsis-App-project/backend/sql/seed.sql
@@ -10,7 +10,7 @@ INSERT INTO Account (id, username, password_hash, email, phone, role, status, cr
 (5, 'user5', '$2a$04$SKvbUbs365RKgNnVcdWs2eHqickqVQnSFveB2s0HUTIPEOmZGxnB2',  'user5@example.com', '0912345682', 'user', 'inactive', '2024-01-01', '2024-06-05 12:00:00', NULL, 0, '192.168.1.5', 'password');
 
 
-INSERT INTO Employee (code, full_name, date_of_birth, gender, address, position, department, start_date, salary, education_level, license_number, emergency_contact_name, emergency_contact_relation, emergency_contact_phone, photo_path, account_id) VALUES
+INSERT INTO Profile (employee_code, full_name, date_of_birth, gender, address, position, department, start_date, salary, education_level, license_number, emergency_contact_name, emergency_contact_relation, emergency_contact_phone, photo_path, account_id) VALUES
 ('EMP001', 'Nguyễn Thị Hoa', '1985-03-15', 'Female', '123 Lê Lợi, Q.1, TP.HCM', 'Bác sĩ Nội', 'Nội khoa', '2010-06-01', 25000000.00, 'Đại học Y Hà Nội', 'BS123456', 'Nguyễn Văn Hùng', 'Chồng', '0911222333', '/images/hoa.png', 1),
 ('EMP002', 'Trần Văn Long', '1979-08-21', 'Male', '456 Nguyễn Trãi, Q.5, TP.HCM', 'Trưởng khoa', 'Ngoại khoa', '2005-01-15', 40000000.00, 'Đại học Y Dược TP.HCM', 'BS654321', 'Trần Thị Lan', 'Vợ', '0908333444', '/images/long.png', 2);
 
